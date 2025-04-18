3a8716ea3f8e5e3ed9cca3c061da100aeac64af7
Srishty Mangutte <srishtymangutte@gmail.com>
2025-04-18 20:10:23 +0530

    feat: added trasnaction path

---
+++ src/app/transactions/page.js
@@ -0,0 +1,40 @@
+"use client";
+
+import ExpenseChart from "../components/ExpenseChart";
+import TransactionForm from "../components/TransactionForm";
+import TransactionList from "../components/TransactionList";
+import api from "@/lib/axios";
+import { useEffect, useState } from "react";
+
+export default function TransactionsPage() {
+  const [transactions, setTransactions] = useState([]);
+  const [editingTransaction, setEditingTransaction] = useState(null);
+
+  const fetchTransactions = async () => {
+    const res = await api.get("/transactions");
+    setTransactions(res.data);
+  };
+
+  const clearEdit = () => setEditingTransaction(null);
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  return (
+    <main className="max-w-3xl mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-6">Transactions</h1>
+      <TransactionForm
+        fetchTransactions={fetchTransactions}
+        editingTransaction={editingTransaction}
+        clearEdit={clearEdit}
+      />
+      <TransactionList
+        transactions={transactions}
+        fetchTransactions={fetchTransactions}
+        setEditingTransaction={setEditingTransaction}
+      />
+      <ExpenseChart transactions={transactions} />
+    </main>
+  );
+}

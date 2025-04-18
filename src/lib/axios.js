b3e29eb47892f0e5427b1987f55bb2ff749e920b
Srishty Mangutte <srishtymangutte@gmail.com>
2025-04-18 22:21:32 +0530

    feat: added backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: "https://finance-tracker-server-n2yh.onrender.com/api",
 });
 
 export default api;
